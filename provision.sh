5f9cbb02c310e3fdddf3870736aaf54d00ca36d3
curious725 <curious725@gmail.com>
2017-06-15 11:57:15 +0300

    copy our default configuration

--- provision.sh
+++ provision.sh
@@ -2,7 +2,11 @@ apt-get -y update
 
 apt-get -y install nginx
 
-rm -rf /usr/share/nginx/www
-ln -s /vagrant/www /usr/share/nginx/
+rm -rf /etc/nginx/sites-enabled
+cp -r /vagrant/sites-enabled /etc/nginx
+
+
+# rm -rf /usr/share/nginx/www
+# ln -s /vagrant/www /usr/share/nginx/
 
 service nginx start
