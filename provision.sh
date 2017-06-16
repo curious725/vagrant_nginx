138c59f24a854c95545416f6f53b4375ea15dade
curious725 <curious725@gmail.com>
2017-06-16 11:33:03 +0300

    add default nginx config (ubuntu 14.04)

--- provision.sh
+++ provision.sh
@@ -2,11 +2,11 @@ apt-get -y update
 
 apt-get -y install nginx
 
-rm -rf /etc/nginx/sites-enabled
-cp -r /vagrant/sites-enabled /etc/nginx
+# rm -rf /etc/nginx/sites-enabled
+# cp -r /vagrant/sites-enabled /etc/nginx
 
 
-# rm -rf /usr/share/nginx/www
-# ln -s /vagrant/www /usr/share/nginx/
+rm -rf /usr/share/nginx/html/
+ln -s /vagrant/www/ /usr/share/nginx/html
 
 service nginx start
