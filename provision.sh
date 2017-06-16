aa64e0bcec3dfaef4dd459fa8dbdb0ae070a3d2d
curious725 <curious725@gmail.com>
2017-06-16 11:36:03 +0300

    delete default sites-enabled folder and copy our

--- provision.sh
+++ provision.sh
@@ -2,8 +2,8 @@ apt-get -y update
 
 apt-get -y install nginx
 
-# rm -rf /etc/nginx/sites-enabled
-# cp -r /vagrant/sites-enabled /etc/nginx
+rm -rf /etc/nginx/sites-enabled
+cp -r /vagrant/sites-enabled /etc/nginx
 
 
 rm -rf /usr/share/nginx/html/
